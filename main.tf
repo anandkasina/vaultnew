4a438fde1bcda6741366c91e004fb5c6bd9153e2
anandkasina <106368078+anandkasina@users.noreply.github.com>
2022-07-07 16:08:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,18 +14,50 @@ resource "vault_aws_secret_backend_role" "role" {
   backend = vault_aws_secret_backend.aws.path
   name    = "test2"
   credential_type = "iam_user"
-  policy_arns = "[arn:aws:iam::aws:policy/AdministratorAccess,arn:aws:iam::aws:policy/AmazonS3FullAccess]"
 
   policy_document = <<EOT
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "*",
-            "Resource": "*"
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iam:CreateAccessKey",
+        "iam:DeleteAccessKey",
+        "iam:GetUser",
+        "iam:DeleteUser",
+        "iam:ListAccessKeys",
+        "iam:ListAttachedUserPolicies",
+        "iam:ListGroupsForUser",
+        "iam:ListUserPolicies",
+        "iam:RemoveUserFromGroup"
+      ],
+      "Resource": [
+        "arn:aws:iam::ACCOUNT-ID-WITHOUT-HYPHENS:user/vault-root-*",
+        "arn:aws:iam::ACCOUNT-ID-WITHOUT-HYPHENS:user/root-for-vault"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iam:AttachUserPolicy",
+        "iam:CreateUser",
+        "iam:DeleteUserPolicy",
+        "iam:DetachUserPolicy",
+        "iam:PutUserPolicy"
+      ],
+      "Resource": [
+        "arn:aws:iam::${AWS_ACCOUNT_ID}:user/vault-root-*"
+      ],
+      "Condition": {
+        "StringEquals": {
+          "iam:PermissionsBoundary": [
+            "arn:aws:iam::ACCOUNT-ID-WITHOUT-HYPHENS:policy/vault-aws-permission-boundary"
+          ]
         }
-    ]
+      }
+    }
+  ]
 }
 EOT
 }
